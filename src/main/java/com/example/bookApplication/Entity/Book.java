4f767d0155b42fb32bc505ca4fdf26a720f58208
rakshitha2708 <98762175+rakshitha270@users.noreply.github.com>
2024-10-05 12:40:23 +0530

    Update @GeneratedValue

--- src/main/java/com/example/bookApplication/Entity/Book.java
+++ src/main/java/com/example/bookApplication/Entity/Book.java
@@ -2,6 +2,7 @@ package com.example.bookApplication.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String author;
